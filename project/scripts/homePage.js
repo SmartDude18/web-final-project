131b7b5f2083ee2d8bf1fdab28c8cea374e2e45d
SmartDude18 <alexanderh1027@gmail.com>
2025-07-30 18:49:32 -0600

    update

--- project/scripts/homePage.js
+++ project/scripts/homePage.js
@@ -1,5 +1,5 @@
 //https://zenquotes.io/api/today
-
+const quote = document.getElementById("quoteSpot");
 let apiUrl = 'https://dummyjson.com/quotes';
     // Make a GET request
     fetch(apiUrl)
@@ -14,8 +14,38 @@ let apiUrl = 'https://dummyjson.com/quotes';
     })
     .then(data => {
         console.log(data);
-        //const quoteData = JSON.parse(JSON.stringify(data));
+        const quoteData = JSON.parse(JSON.stringify(data));
+        let quoteIndex = 0;
+        while(true)
+        {
+            if(quoteData.quotes[quoteIndex].quote.toLowerCase().includes("god"))
+            {
+                quoteIndex++;
+            }
+            else
+            {
+                quote.textContent = quoteData.quotes[quoteIndex].quote;
+                break;
+            }
+        }
     })
     .catch(error => {
         console.error('Error:', error);
     });
+
+
+
+
+    let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("mySlides");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
