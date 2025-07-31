834d5f785cb4cac281a82ea4048abbb7bec7574e
SmartDude18 <alexanderh1027@gmail.com>
2025-07-30 20:05:55 -0600

    update main page

--- project/scripts/homePage.js
+++ project/scripts/homePage.js
@@ -1,42 +1,40 @@
 //https://zenquotes.io/api/today
 const quote = document.getElementById("quoteSpot");
+let slideIndex = 0;
 let apiUrl = 'https://dummyjson.com/quotes';
-    // Make a GET request
-    fetch(apiUrl)
-    .then(response => {
-        console.log("SWAWS");
-        if (!response.ok) {
-            console.log(response.status);
-        throw new Error('Network response was not ok');
-        }
-        
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-        const quoteData = JSON.parse(JSON.stringify(data));
-        let quoteIndex = 0;
-        while(true)
+// Make a GET request
+fetch(apiUrl)
+.then(response => {
+    console.log("SWAWS");
+    if (!response.ok) {
+        console.log(response.status);
+    throw new Error('Network response was not ok');
+    }
+    
+    return response.json();
+})
+.then(data => {
+    console.log(data);
+    const quoteData = JSON.parse(JSON.stringify(data));
+    let quoteIndex = 0;
+    while(true)
+    {
+        if(quoteData.quotes[quoteIndex].quote.toLowerCase().includes("god"))
         {
-            if(quoteData.quotes[quoteIndex].quote.toLowerCase().includes("god"))
-            {
-                quoteIndex++;
-            }
-            else
-            {
-                quote.textContent = quoteData.quotes[quoteIndex].quote;
-                break;
-            }
+            quoteIndex++;
         }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+        else
+        {
+            quote.textContent = quoteData.quotes[quoteIndex].quote;
+            break;
+        }
+    }
+})
+.catch(error => {
+    console.error('Error:', error);
+});
 
 
-
-
-    let slideIndex = 0;
 showSlides();
 
 function showSlides() {
@@ -45,7 +43,7 @@ function showSlides() {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  if (slideIndex == slides.length) {slideIndex = 0}
+  slides[slideIndex].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
